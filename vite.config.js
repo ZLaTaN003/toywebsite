c63b07c613279620b40013c463436418626556e3
AdithyaKrishna <adithyakrishnanszz@gmail.com>
2026-06-30 19:10:14 +0530

    fix cart in vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         listing: resolve(__dirname, "listing.html"),
         detail: resolve(__dirname, "detail.html"),
+        cart: resolve(__dirname, "cart.html"),
       },
     },
   },
